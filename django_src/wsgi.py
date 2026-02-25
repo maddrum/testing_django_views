22f4699ba4cf077e7cc4007aca3360c3b2b99274
maddrum <maddrum9@gmail.com>
2026-02-25 20:32:19 +0200

    wip 6

--- django_src/wsgi.py
+++ django_src/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_django_views.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
 
 application = get_wsgi_application()
--- docker/run-server.sh
+++ docker/run-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-python3 -W always $DJANGO_PROJECT_DIR/manage.py runserver_plus 0.0.0.0:8000  --noreload
+python -W always manage.py runserver 0.0.0.0:8000  --noreload
