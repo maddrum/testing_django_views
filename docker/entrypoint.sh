06e3a6a9a289500e27d1effeed59994dfd45024c
maddrum <maddrum9@gmail.com>
2026-02-25 19:46:03 +0200

    wip 5

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,10 @@ set -o errexit
 set -o pipefail
 set -e
 
-pip3 install -r requirements.txt
-python manage.py migrate --noinput
+pip install -r requirements.txt --user
+
+if [ -f "manage.py" ]; then
+  python manage.py migrate --noinput
+fi
 
 exec "$@"
--- docker/run-server.sh
+++ docker/run-server.sh
@@ -1,36 +1,4 @@
 #!/usr/bin/env bash
-#
-# Cookiecutter Django CMS with Docker support
-# This cookiecutter provides a production ready Django CMS template using Docker.
-#
-# https://github.com/guma44/cookiecutter-django-cms-docker
-#
 
 
-# end when error
-set -e
-# raise error when variable is unset
-set -u
-# raise error when in pipe
-set -o pipefail
-
-
-if (( $# != 1 )); then
-    echo "Ussage: run_server.sh [local|gunicorn]"
-	exit
-fi
-
-if [ "$1" == "local" ]; then
-	echo "Running local development server"
-	echo "Python warnings are enabled"
-	export WERKZEUG_DEBUG_PIN="off"
-#	python3 $DJANGO_PROJECT_DIR/manage.py migrate
-	python3 -W always $DJANGO_PROJECT_DIR/manage.py runserver_plus 0.0.0.0:8000  --noreload
-elif [ "$1" == "gunicorn" ]; then
-	echo "Running Gunicorn"
-	python3 manage.py migrate
-	python3 manage.py collectstatic --noinput
-	gunicorn -c "python:config.gunicorn"
-else
-	echo "Usage: run_server.sh [local|gunicorn]"
-fi
+python3 -W always $DJANGO_PROJECT_DIR/manage.py runserver_plus 0.0.0.0:8000  --noreload
