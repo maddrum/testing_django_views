62dc94d0efda185aee3b4003c30ff7bda7b2a36f
maddrum <hr.trendafilov@gmail.com>
2026-02-27 11:47:55 +0200

    wip 102

--- django_src/apps/working_times/tests/tests_forms.py
+++ django_src/apps/working_times/tests/tests_forms.py
@@ -1,6 +1,9 @@
+import datetime
+
 from django.test import TestCase
 
 from apps.working_times.forms import WorkingTimeForm
+from apps.working_times.settings import MIN_WORKING_TIME_DURATION_HOURS, REPRESENTATION_TIME_FORMAT
 
 
 class WorkingTimeFormTests(TestCase):
@@ -16,6 +19,12 @@ class WorkingTimeFormTests(TestCase):
         form = WorkingTimeForm(data=self.form_data)
         self.assertTrue(form.is_valid(), form.errors)
 
+        form_data = self.form_data.copy()
+        form_data["to_time"] = "23:59"
+        form_data["from_time"] = "00:00"
+        form = WorkingTimeForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
     def test_valid_to_midnight(self):
         form_data = self.form_data.copy()
         form_data["from_time"] = "10:00"
@@ -28,6 +37,23 @@ class WorkingTimeFormTests(TestCase):
         form_data["to_time"] = "00:00"
         form = WorkingTimeForm(data=form_data)
         self.assertTrue(form.is_valid(), form.errors)
+
+    def test_valid_min_working_times(self):
+        form_data = self.form_data.copy()
+        to_time = datetime.datetime.combine(datetime.date.today(), datetime.time(18, 0))
+        from_time = to_time - datetime.timedelta(hours=MIN_WORKING_TIME_DURATION_HOURS) - datetime.timedelta(minutes=1)
+        form_data["to_time"] = to_time.strftime(REPRESENTATION_TIME_FORMAT)
+        form_data["from_time"] = from_time.strftime(REPRESENTATION_TIME_FORMAT)
+        form = WorkingTimeForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+        form_data = self.form_data.copy()
+        to_time = datetime.datetime.combine(datetime.date.today(), datetime.time(18, 0))
+        from_time = to_time - datetime.timedelta(hours=MIN_WORKING_TIME_DURATION_HOURS)
+        form_data["to_time"] = to_time.strftime(REPRESENTATION_TIME_FORMAT)
+        form_data["from_time"] = from_time.strftime(REPRESENTATION_TIME_FORMAT)
+        form = WorkingTimeForm(data=form_data)
+        self.assertTrue(form.is_valid())
 
     def test_invalid_missing_to_time(self):
         form_data = self.form_data.copy()
@@ -76,28 +102,53 @@ class WorkingTimeFormTests(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn("End time must be greater than start time", form.errors["__all__"], form.errors)
 
-    def test_invalid_start_time_greater_than_end_time(self):
         form_data = self.form_data.copy()
-        form_data["from_time"] = "19:00"
+        form_data["to_time"] = "00:00"
+        form_data["from_time"] = "00:00"
         form = WorkingTimeForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn("End time must be greater than start time", form.errors["__all__"], form.errors)
 
         form_data = self.form_data.copy()
-        form_data["from_time"] = "18:00"
+        form_data["to_time"] = "24:00"
+        form_data["from_time"] = "24:00"
         form = WorkingTimeForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn("End time must be greater than start time", form.errors["__all__"], form.errors)
+
+    def test_invalid_start_time_greater_than_end_time(self):
+        error_message = "End time must be greater than start time"
+
+        form_data = self.form_data.copy()
+        form_data["from_time"] = "19:00"
+        form = WorkingTimeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn(error_message, form.errors["__all__"], form.errors)
+
+        form_data = self.form_data.copy()
+        form_data["from_time"] = "18:00"
+        form = WorkingTimeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn(error_message, form.errors["__all__"], form.errors)
 
         form_data = self.form_data.copy()
         form_data["to_time"] = "00:00"
         form_data["from_time"] = "23:59"
         form = WorkingTimeForm(data=form_data)
         self.assertFalse(form.is_valid())
+        self.assertIn(error_message, form.errors["__all__"], form.errors)
 
     def test_invalid_total_duration_should_be_more_than_min_hours(self):
-        # TODO
-        pass
+        error_message = f"Total duration should be at least {MIN_WORKING_TIME_DURATION_HOURS} hour"
+
+        form_data = self.form_data.copy()
+        to_time = datetime.datetime.combine(datetime.date.today(), datetime.time(18, 0))
+        from_time = to_time - datetime.timedelta(hours=MIN_WORKING_TIME_DURATION_HOURS) + datetime.timedelta(minutes=1)
+        form_data["to_time"] = to_time.strftime(REPRESENTATION_TIME_FORMAT)
+        form_data["from_time"] = from_time.strftime(REPRESENTATION_TIME_FORMAT)
+        form = WorkingTimeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn(error_message, form.errors["__all__"], form.errors)
 
     def test_note_is_not_required(self):
         form_data = self.form_data.copy()
